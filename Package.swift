50f58b49df892e7ea8f0bde9e287b47b4de206cc
Nathaniel Curnick <nathaniel.c@naurt.com>
2022-10-19 10:28:12 +0100

    Tried changing package

--- Package.swift
+++ Package.swift
@@ -3,12 +3,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "Naurt.swift",
+    name: "NaurtSDK",
     platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Naurt.swift",
+            name: "NaurtSDK",
             targets: ["NaurtSDK"]),
     ],
     dependencies: [
